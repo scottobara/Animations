dcf31540edccaf1c55d053259b1e94ea38af8f31
Scott Obara <scott@rubbscreations.com>
2021-01-16 20:35:46 +1100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Scott Obara on 16/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
